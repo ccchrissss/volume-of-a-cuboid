640f23f97bbda76bc945ff18a9cbacdbe4503aee
Chris Nelson <chrisnelson7791@gmail.com>
2023-09-25 20:30:32 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,14 @@
+class Kata {
+
+  static getVolumeOfCuboid(length, width, height) {
+      return length * width * height
+  }
+  
+}
+
+// parameter: length, width, height
+// return: volume 
+// e.g. getVolumeOfCuboid(5,5,5)
+// results in 125
+
+// return the length * width * height
